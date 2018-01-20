cf91ddd8a2b5b7995b0a4aefd70900f336548727
Ivanq <imachug@yandex.ru>
2018-01-20 11:24:01 +0300

    Add package.json and main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+module.exports = {
+	ZeroPage: require("./ZeroPage"),
+	ZeroFrame: require("./ZeroFrame"),
+
+	ZeroAuth: require("./ZeroAuth"),
+	ZeroBigFiles: require("./ZeroBigFiles"),
+	ZeroDB: require("./ZeroDB"),
+	ZeroFS: require("./ZeroFS"),
+	ZeroID: require("./ZeroID"),
+	ZeroOptional: require("./ZeroOptional"),
+	ZeroOptionalProxy: require("./ZeroOptionalProxy")
+};
